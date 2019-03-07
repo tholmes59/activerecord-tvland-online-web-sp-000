5ae02e2d1e4d1345d681d8f17ff6852ea2c74424
Tom Holmes <tholmes59@gmail.com>
2019-03-07 18:56:55 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,9 +6,9 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
 
-  # def list_roles
-  #   characters.collect do |character|
-  #     "#{character.name} - #{character.show.name}"
-  #   end
-  # end
+  def list_roles
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 end
