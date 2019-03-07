865e5d1b32b0f2e231114777a6f7c5899833fce4
Tom Holmes <tholmes59@gmail.com>
2019-03-07 18:55:33 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,9 +2,9 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
-  # def full_name
-  #   "#{self.first_name} #{self.last_name}"
-  # end
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 
   # def list_roles
   #   characters.collect do |character|
